00f1222c421d7ffff89edb0ca8b773e0e8f968c7
Rajat <you@example.com>
2018-05-05 16:13:40 +0530

    added progess bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import Loader from 'react-loader-advanced'
-
+import { Route } from 'react-router-dom'
+import Student from './student';
 
 class App extends Component {
   state = {
@@ -29,6 +30,10 @@ class App extends Component {
     }));
   };
 
+  handleCard = (id) => {
+    this.props.history.push(`/${id}`)
+  }
+
   componentDidMount(){
     axios
       .get(`https://api.myjson.com/bins/1dlper`)
@@ -41,6 +46,32 @@ class App extends Component {
     if(Object.keys(this.state.students).length == 0){
       return (<Loader show={true} message={'loading'} />)
     }
+    else if (this.props.match.path === "/:number"){
+      return (
+      <div className="card col-md-4 col-sm-6 col-xs-12" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Name: {this.state.students[this.props.match.params.number].name}</h5>
+            <h5 className="card-title">Id: {this.props.match.params.number}</h5>
+          <h5 className="card-title">Total Marks: {Object.keys(this.state.students[this.props.match.params.number].marks).reduce((acc, initital) => {
+            return acc + this.state.students[this.props.match.params.number].marks[initital]
+          }, 0)}</h5>
+
+          <div className="chart">
+          {
+                Object.keys(this.state.students[this.props.match.params.number].marks).map(data => (
+                  <div>
+                    <span>{data}
+                  <div class="progress my-4">
+                      <div class="progress-bar" role="progressbar" style={{ width: `${this.state.students[this.props.match.params.number].marks[data]}%` }} aria-valuenow="25" aria-valuemin="0" aria-valuemax={"" + this.state.students[this.props.match.params.number].marks[data]}>{this.state.students[this.props.match.params.number].marks[data]}%</div>
+                      </div></span>
+                  </div>
+                ))
+          }
+          </div>
+        </div>
+      </div>)
+    }
+    else {
     return (
       <div className="App">
         <header className="sticky-top d-flex p-4">
@@ -51,7 +82,7 @@ class App extends Component {
         <div className='row'>
           {Object.keys(this.state.searchStudent).length == 0 && this.state.search ? <div className="col-md-12 col-sm-12 col-xs-12 d-flex justify-content-center">No Result Found</div> :
           (!this.state.search ? Object.keys(this.state.students) : this.state.searchStudent).map(student => (
-            <div key={student} className="card col-md-4 col-sm-6 col-xs-12" style={{width: "18rem"}}>
+              <div key={student} onClick={this.handleCard.bind(this, student)} className="card col-md-4 col-sm-6 col-xs-12" style={{width: "18rem"}}>
                 <div className="card-body">
                   <h5 className="card-title">Name: {this.state.students[student].name}</h5>
                   <h5 className="card-title">Id: {student}</h5>
@@ -65,6 +96,7 @@ class App extends Component {
         </div>
       </div>
     );
+  }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const Main = () => (
+    <main>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Route path='/:number' component={App} />
+        </Switch>
+    </main>
+)
+
+
+ReactDOM.render(<BrowserRouter><Main /></BrowserRouter>, document.getElementById('root'));
+//registerServiceWorker();
