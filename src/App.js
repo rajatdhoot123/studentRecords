61044aa6305226843d0519f094d29b71f93acdc0
Rajat <you@example.com>
2018-05-05 22:44:05 +0530

    style updated

--- src/App.js
+++ src/App.js
@@ -20,17 +20,16 @@ class App extends Component {
         }
       } else {
         return {
-        sorted: prevState.sorted.sort((a, b) => prevState.students[b].name.localeCompare(prevState.students[a].name)),
-        sortMarksBy: 'asc'
+          sorted: prevState.sorted.sort((a, b) => prevState.students[b].name.localeCompare(prevState.students[a].name)),
+          sortMarksBy: 'asc'
         }
       }
     })
-  }  
+  }
 
   toggleMarks = () => {
     this.setState(prevState => {
-      console.log(prevState,"rpevadf")
-      if(prevState.sortMarksBy === "asc"){
+      if (prevState.sortMarksBy === "asc") {
         return {
           sorted: prevState.sorted.sort((a, b) => Object.keys(prevState.students[a].marks).reduce((acc, initital) => {
             return acc + prevState.students[a].marks[initital]
@@ -54,7 +53,7 @@ class App extends Component {
 
   handleSearch = e => {
     let name = `^${e.target.value}`
-    let regex = new RegExp(name, "gi");  
+    let regex = new RegExp(name, "gi");
     this.setState(prevState => ({
       sorted: Object.keys(prevState.students).filter(student =>
         ("" + prevState.students[student].name).match(regex)
@@ -67,7 +66,7 @@ class App extends Component {
     this.props.history.push(`/${id}`)
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios
       .get(`https://api.myjson.com/bins/1dlper`)
       .then(response => {
@@ -76,65 +75,71 @@ class App extends Component {
   }
 
   render() {
-    let { sorted, sortMarksBy } = this.state
-    if(Object.keys(this.state.students).length == 0){
+    let { sorted } = this.state
+    if (Object.keys(this.state.students).length === 0) {
       return (<Loader show={true} message={'loading'} />)
     }
-    else if (this.props.match.path === "/:number"){
+    else if (this.props.match.path === "/:number") {
       return (
         <div className="d-flex justify-content-center p-4">
           <div className="card" style={{ width: "80%" }}>
-        <div className="card-body">
-          <h5 className="card-title">Name: {this.state.students[this.props.match.params.number].name}</h5>
-            <h5 className="card-title">Id: {this.props.match.params.number}</h5>
-          <h5 className="card-title">Total Marks: {Object.keys(this.state.students[this.props.match.params.number].marks).reduce((acc, initital) => {
-            return acc + this.state.students[this.props.match.params.number].marks[initital]
-          }, 0)}</h5>
+            <div className="card-body">
+              <h5 className="card-title">Name: {this.state.students[this.props.match.params.number].name}</h5>
+              <h5 className="card-title">Id: {this.props.match.params.number}</h5>
+              <h5 className="card-title">Total Marks: {Object.keys(this.state.students[this.props.match.params.number].marks).reduce((acc, initital) => {
+                return acc + this.state.students[this.props.match.params.number].marks[initital]
+              }, 0)}</h5>
 
-          <div className="chart">
-          {
-                Object.keys(this.state.students[this.props.match.params.number].marks).map(data => (
-                  <div key={data} className="d-flex">
+              <div className="chart">
+                {
+                  Object.keys(this.state.students[this.props.match.params.number].marks).map(data => (
+                    <div key={data} className="d-flex">
                       <p>{data}</p>
                       <div className="progress mx-4 w-75 mt-1">
-                      <div className="progress-bar" role="progressbar" style={{ width: `${this.state.students[this.props.match.params.number].marks[data]}%` }} aria-valuenow="25" aria-valuemin="0" aria-valuemax={"" + this.state.students[this.props.match.params.number].marks[data]}>{this.state.students[this.props.match.params.number].marks[data]}%</div>
+                        <div className="progress-bar" role="progressbar" style={{ width: `${this.state.students[this.props.match.params.number].marks[data]}%` }} aria-valuenow="25" aria-valuemin="0" aria-valuemax={"" + this.state.students[this.props.match.params.number].marks[data]}>{this.state.students[this.props.match.params.number].marks[data]}%</div>
                       </div>
-                  </div>
-                ))
-          }
-          </div>
-        </div>
+                    </div>
+                  ))
+                }
+              </div>
             </div>
-      </div>)
+          </div>
+        </div>)
     }
     else {
-    return (
-      <div className="App">
-        <header className="sticky-top d-flex p-4">
-            <input className="form-control mx-2" type="search" onChange={this.handleSearch.bind(this)} placeholder="Search" aria-label="Search" />
-          <button className="btn btn-primary mx-2" onClick={this.toggleSort.bind(this)}>Sort by Name</button>
-          <button className="btn btn-primary mx-2" onClick={this.toggleMarks}>Sort by Marks</button>
-        </header>
-        <div className="container-fluid" >
-        <div className='row'>
-          {Object.keys(this.state.sorted).length == 0 && this.state.search ? <div className="col-md-12 col-sm-12 col-xs-12 d-flex justify-content-center">No Result Found</div> :
-            ((!sorted.length) ? !this.state.search ? Object.keys(this.state.students) : sorted : sorted).map(student => (
-              <div key={student} onClick={this.handleCard.bind(this, student)} className="card col-md-4 col-sm-6 col-xs-12" style={{width: "18rem"}}>
-                <div className="card-body">
-                  <h5 className="card-title">Name: {this.state.students[student].name}</h5>
-                  <h5 className="card-title">Id: {student}</h5>
-                  <h5 className="card-title">Total Marks: {Object.keys(this.state.students[student].marks).reduce((acc,initital) => {
-                  return acc + this.state.students[student].marks[initital]
-                  },0)}</h5>
-                </div>
-          </div>)
-          )
-          }
+      return (
+        <div className="App">
+          <header className="sticky-top row p-3">
+            <div className="col-md-8 my-2">
+              <input className="form-control" type="search" onChange={this.handleSearch.bind(this)} placeholder="Search" aria-label="Search" />
+            </div>
+            <div className="col-md-2 my-2">
+              <button className="btn btn-primary btn-block" onClick={this.toggleSort.bind(this)}>Sort by Name</button>
+            </div>
+            <div className="col-md-2 my-2">
+              <button className="btn btn-primary btn-block" onClick={this.toggleMarks}>Sort by Marks</button>
+            </div>
+          </header>
+          <div className="container-fluid" >
+            <div className='row'>
+              {Object.keys(this.state.sorted).length === 0 && this.state.search ? <div className="col-md-12 col-sm-12 col-xs-12 d-flex justify-content-center">No Result Found</div> :
+                ((!sorted.length) ? !this.state.search ? Object.keys(this.state.students) : sorted : sorted).map(student => (
+                  <div key={student} onClick={this.handleCard.bind(this, student)} className="card col-md-4 col-sm-6 col-xs-12" >
+                    <div className="card-body">
+                      <h5 className="card-title">Name: {this.state.students[student].name}</h5>
+                      <h5 className="card-title">Id: {student}</h5>
+                      <h5 className="card-title">Total Marks: {Object.keys(this.state.students[student].marks).reduce((acc, initital) => {
+                        return acc + this.state.students[student].marks[initital]
+                      }, 0)}</h5>
+                    </div>
+                  </div>)
+                )
+              }
+            </div>
+          </div>
         </div>
-        </div>
-      </div>
-    );
-  }
+      );
+    }
   }
 }
 
