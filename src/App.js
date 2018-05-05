b57cc7e2c1647d722a157b1cb53050b4fa38f3df
Rajat <you@example.com>
2018-05-05 15:26:20 +0530

    search implemented

--- src/App.js
+++ src/App.js
@@ -7,8 +7,27 @@ import Loader from 'react-loader-advanced'
 
 class App extends Component {
   state = {
-    students: {}
+    students: {},
+    searchStudent: [],
+    search: false
   }
+
+  toggleSort = () => {
+    this.setState(prevState => {
+      students: Object.keys(prevState.students).sort().reduce((r, k) => (r[k] = prevState.students[k], r), {});
+    })
+  }
+  
+  handleSearch = e => {
+    let x = `^${e.target.value}`
+    let regex = new RegExp(x, "gi");  
+    this.setState(prevState => ({
+      searchStudent: Object.keys(prevState.students).filter(student =>
+        ("" + prevState.students[student].name).match(regex)
+      ),
+      search: true
+    }));
+  };
 
   componentDidMount(){
     axios
@@ -24,12 +43,14 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="sticky-top d-flex p-4">
+            <input className="form-control mx-2" type="search" onChange={this.handleSearch.bind(this)} placeholder="Search" aria-label="Search" />
+          <button className="btn btn-primary mx-2" onClick={this.toggleSort.bind(this)}>Sort</button>
+          <button className="btn btn-primary mx-2">Asending</button>
         </header>
         <div className='row'>
-          {Object.keys(this.state.students).map(student => (
+          {Object.keys(this.state.searchStudent).length == 0 && this.state.search ? <div className="col-md-12 col-sm-12 col-xs-12 d-flex justify-content-center">No Result Found</div> :
+          (!this.state.search ? Object.keys(this.state.students) : this.state.searchStudent).map(student => (
             <div key={student} className="card col-md-4 col-sm-6 col-xs-12" style={{width: "18rem"}}>
                 <div className="card-body">
                   <h5 className="card-title">Name: {this.state.students[student].name}</h5>
@@ -39,7 +60,8 @@ class App extends Component {
                   },0)}</h5>
                 </div>
           </div>)
-          )}
+          )
+          }
         </div>
       </div>
     );
